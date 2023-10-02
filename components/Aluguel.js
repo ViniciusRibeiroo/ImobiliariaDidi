3fdd24cd94bad425df0051cd3e67407418fd8a68
jpsbat <jpsbat56@gmail.com>
2023-10-02 11:59:06 -0300

    mudancas

--- components/Aluguel.js
+++ components/Aluguel.js
@@ -1,12 +1,75 @@
-import { Image, View, Text } from 'react-native';
+import { Image, View, StyleSheet, FlatList, Text } from 'react-native';
 
-export default function Aluguel () {
+import centro from '../assets/casas_aluguel/Centro.png';
+import saomatheus from '../assets/casas_aluguel/SaoMatheus.png';
+import jdbelavista from '../assets/casas_aluguel/JdBelaVista.png';
+import condmoncoes from '../assets/casas_aluguel/CondMoncoes.png';
+
+export default function Compra() {
   return (
-  <View style={{ flex: 1, alignItems: 'center', padding: 20 }}>
-    <Image
-      style={{width: 222, height: 97, marginBottom: 10 }}
-      source={require("../assets/Logo_didi.png")}
-    />
-  </View>
+    <View style={{ flex: 1, padding: 20 }}>
+      <View style={estilos.logoContainer}>
+        <Image style={estilos.logo}
+          source={require("../assets/Logo_didi.png")}
+        />
+      </View>
+      <FlatList
+        style={estilos.flatList}
+        data={[
+          {
+            nome: 'Centro',
+            imagem: centro,
+          },
+          {
+            nome: "São Matheus",
+            imagem: saomatheus,
+          },
+          {
+            nome: "Jd Bela Vista",
+            imagem: jdbelavista,
+          },
+          {
+            nome: "Cond Monções",
+            imagem: condmoncoes,
+          }
+        ]}
+        renderItem={({ item }) => (
+          <View style={estilos.itemContainer}>
+            <Image style={estilos.imagem} source={item.imagem} />
+            <Text style={estilos.nome}>{item.nome}</Text>            
+          </View>
+        )}
+        keyExtractor={(item) => item.nome}
+      />
+    </View>
   );
 }
+
+const estilos = StyleSheet.create({
+  flatList: {
+    justifyContent: 'flex-start',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  imagem: {
+    width: 46,
+    height: 46,
+  },
+  nome: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 11,
+    color: "#464646",
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  logo: {
+    width: 222,
+    height: 97,
+  }
+});
--- components/Compra.js
+++ components/Compra.js
@@ -1,12 +1,75 @@
-import { Image, View } from 'react-native';
+import { Image, View, StyleSheet, FlatList, Text } from 'react-native';
 
-export default function Compra () {
+import montroyal from '../assets/casas_compra/MontRoyal.png';
+import altosavecuia from '../assets/casas_compra/AltosAvecuia.png';
+import villageamerica from '../assets/casas_compra/VillageAmerica.png';
+import jardimexcelsior from '../assets/casas_compra/JardimExcelsior.png';
+
+export default function Compra() {
   return (
-  <View style={{ flex: 1, alignItems: 'center', padding: 20 }}>
-    <Image
-      style={{width: 222, height: 97, marginBottom: 10 }}
-      source={require("../assets/Logo_didi.png")}
-    />
-  </View>
+    <View style={{ flex: 1, padding: 20 }}>
+      <View style={estilos.logoContainer}>
+        <Image style={estilos.logo}
+          source={require("../assets/Logo_didi.png")}
+        />
+      </View>
+      <FlatList
+        style={estilos.flatList}
+        data={[
+          {
+            nome: 'Mont Royal',
+            imagem: montroyal,
+          },
+          {
+            nome: "Altos de Avecuia",
+            imagem: altosavecuia,
+          },
+          {
+            nome: "Village América",
+            imagem: villageamerica,
+          },
+          {
+            nome: "Jardim Excelsior",
+            imagem: jardimexcelsior,
+          }
+        ]}
+        renderItem={({ item }) => (
+          <View style={estilos.itemContainer}>
+            <Image style={estilos.imagem} source={item.imagem} />
+            <Text style={estilos.nome}>{item.nome}</Text>            
+          </View>
+        )}
+        keyExtractor={(item) => item.nome}
+      />
+    </View>
   );
 }
+
+const estilos = StyleSheet.create({
+  flatList: {
+    justifyContent: 'flex-start',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  imagem: {
+    width: 46,
+    height: 46,
+  },
+  nome: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 11,
+    color: "#464646",
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  logo: {
+    width: 222,
+    height: 97,
+  }
+});
