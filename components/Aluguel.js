9b31ef13c7fb1b723bbb55be081d1340c9a18972
jpsbat <jpsbat56@gmail.com>
2023-10-02 18:00:50 -0300

    mudancas

--- components/Aluguel.js
+++ components/Aluguel.js
@@ -5,12 +5,12 @@ import saomatheus from '../assets/casas_aluguel/SaoMatheus.png';
 import jdbelavista from '../assets/casas_aluguel/JdBelaVista.png';
 import condmoncoes from '../assets/casas_aluguel/CondMoncoes.png';
 
-export default function Compra() {
+export default function Aluguel() {
   return (
     <View style={{ flex: 1, padding: 20 }}>
       <View style={estilos.logoContainer}>
         <Image style={estilos.logo}
-          source={require("../assets/Logo_didi.png")}
+          source={require("../assets/Logo.png")}
         />
       </View>
       <FlatList
@@ -18,25 +18,42 @@ export default function Compra() {
         data={[
           {
             nome: 'Centro',
+            descricao: 'Sala comercial',
+            preco: 'R$850,00',
+            conteudo: '1 banheiro',
             imagem: centro,
           },
           {
-            nome: "São Matheus",
+            nome: 'São Matheus',
+            descricao: 'Casa em Condomínio',
+            preco: 'R$2.000,00',
+            conteudo: '3 dormitórios - 2 banheiros',
             imagem: saomatheus,
           },
           {
-            nome: "Jd Bela Vista",
+            nome: 'Jd Bela Vista',
+            descricao: 'Casa mobiliada',
+            preco: 'R$3.000,00',
+            conteudo: '3 dormitórios - 3 banheiros',
             imagem: jdbelavista,
           },
           {
-            nome: "Cond Monções",
+            nome: 'Cond Monções',
+            descricao: 'Casa em Condomínio',
+            preco: 'R$4.000,00',
+            conteudo: '3 dormitórios - 2 banheiros',
             imagem: condmoncoes,
           }
         ]}
         renderItem={({ item }) => (
           <View style={estilos.itemContainer}>
             <Image style={estilos.imagem} source={item.imagem} />
-            <Text style={estilos.nome}>{item.nome}</Text>            
+            <View style={estilos.textContainer}>
+              <Text style={estilos.nome}>{item.nome}</Text>
+              <Text style={estilos.descricao}>{item.descricao}</Text>
+              <Text style={estilos.preco}>{item.preco}</Text>
+              <Text style={estilos.conteudo}>{item.conteudo}</Text>  
+            </View>          
           </View>
         )}
         keyExtractor={(item) => item.nome}
@@ -49,24 +66,50 @@ const estilos = StyleSheet.create({
   flatList: {
     justifyContent: 'flex-start',
   },
+  textContainer: {
+  flexDirection: 'column',
+  marginLeft: 25,
+  },
   itemContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
+    flexDirection: "row",
+    borderBottomWidth: 10,
+    borderBottomColor: "#ECECEC",
+    paddingVertical: 16,
+    marginHorizontal: 16,
+    alignItems: "center",
   },
   imagem: {
-    width: 46,
-    height: 46,
+    width: 135,
+    height: 110,
   },
   nome: {
-    fontSize: 16,
+    fontSize: 18,
     lineHeight: 26,
-    marginLeft: 11,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  descricao: {
+    fontSize: 10,
+    lineHeight: 15,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: "#464646",
+  },
+  preco: {
+    fontSize: 10,
+    lineHeight: 26,
+    textAlign: 'center',
+    color: "#464646",
+  },
+  conteudo: {
+    fontSize: 10,
+    lineHeight: 15,
+    textAlign: 'center',
     color: "#464646",
   },
   logoContainer: {
     alignItems: 'center',
-    marginBottom: 10,
+    marginBottom: 20,
   },
   logo: {
     width: 222,
--- components/Compra.js
+++ components/Compra.js
@@ -10,7 +10,7 @@ export default function Compra() {
     <View style={{ flex: 1, padding: 20 }}>
       <View style={estilos.logoContainer}>
         <Image style={estilos.logo}
-          source={require("../assets/Logo_didi.png")}
+          source={require("../assets/Logo.png")}
         />
       </View>
       <FlatList
@@ -18,25 +18,42 @@ export default function Compra() {
         data={[
           {
             nome: 'Mont Royal',
+            descricao: 'Apartamento',
+            preco: 'R$135.000,00',
+            conteudo: '1 dormitório - 1 banheiro',
             imagem: montroyal,
           },
           {
-            nome: "Altos de Avecuia",
+            nome: 'Altos do Avecuia',
+            descricao: 'Apartamento na planta',
+            preco: 'R$169.990,00',
+            conteudo: '2 dormitórios - 1 banheiro',
             imagem: altosavecuia,
           },
           {
-            nome: "Village América",
+            nome: 'Village América',
+            descricao: 'Casa em Condomínio',
+            preco: 'R$295.000,00',
+            conteudo: '2 dormitórios - 2 banheiros',
             imagem: villageamerica,
           },
           {
-            nome: "Jardim Excelsior",
+            nome: 'Jardim Excelsior',
+            descricao: 'Sobrado',
+            preco: 'R$320.000,00',
+            conteudo: '2 dormitórios - 2 banheiros',
             imagem: jardimexcelsior,
           }
         ]}
         renderItem={({ item }) => (
           <View style={estilos.itemContainer}>
             <Image style={estilos.imagem} source={item.imagem} />
-            <Text style={estilos.nome}>{item.nome}</Text>            
+            <View style={estilos.textContainer}>
+              <Text style={estilos.nome}>{item.nome}</Text>
+              <Text style={estilos.descricao}>{item.descricao}</Text>
+              <Text style={estilos.preco}>{item.preco}</Text>
+              <Text style={estilos.conteudo}>{item.conteudo}</Text>  
+            </View>          
           </View>
         )}
         keyExtractor={(item) => item.nome}
@@ -49,24 +66,50 @@ const estilos = StyleSheet.create({
   flatList: {
     justifyContent: 'flex-start',
   },
+  textContainer: {
+  flexDirection: 'column',
+  marginLeft: 25,
+  },
   itemContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
+    flexDirection: "row",
+    borderBottomWidth: 10,
+    borderBottomColor: "#ECECEC",
+    paddingVertical: 16,
+    marginHorizontal: 16,
+    alignItems: "center",
   },
   imagem: {
-    width: 46,
-    height: 46,
+    width: 135,
+    height: 110,
   },
   nome: {
-    fontSize: 16,
+    fontSize: 18,
     lineHeight: 26,
-    marginLeft: 11,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  descricao: {
+    fontSize: 10,
+    lineHeight: 15,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: "#464646",
+  },
+  preco: {
+    fontSize: 10,
+    lineHeight: 26,
+    textAlign: 'center',
+    color: "#464646",
+  },
+  conteudo: {
+    fontSize: 10,
+    lineHeight: 15,
+    textAlign: 'center',
     color: "#464646",
   },
   logoContainer: {
     alignItems: 'center',
-    marginBottom: 10,
+    marginBottom: 20,
   },
   logo: {
     width: 222,
