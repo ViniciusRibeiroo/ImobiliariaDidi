19aca9268b64882966e4e9406f0a27c367646a28
jpsbat <jpsbat56@gmail.com>
2023-10-03 19:43:12 -0300

    mudancas

--- components/Contato.js
+++ components/Contato.js
@@ -1,12 +1,31 @@
-import { Image, View } from 'react-native';
+import { Image, View, ImageBackground, StyleSheet } from 'react-native';
+
+import fundo from '../assets/fundo.png';
 
 export default function Contato () {
   return (
-  <View style={{ flex: 1, alignItems: 'center', padding: 20 }}>
+  <View style={{ flex: 1 }}>
+  <ImageBackground source={fundo} style={estilos.fundo}>
     <Image
-      style={{width: 222, height: 97, marginBottom: 10 }}
-      source={require("../assets/Logo_didi.png")}
+      style={estilos.logo}
+      source={require("../assets/Logo.png")}
     />
+  </ImageBackground>
   </View>
   );
 }
+
+const estilos = StyleSheet.create({
+  fundo: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+    width: '100%'
+  },
+  logo: {
+    width: 222,
+    height: 97,
+    marginTop: 20
+  },
+});
